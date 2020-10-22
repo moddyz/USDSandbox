d1573f07e83110152cd623edd09d621566ffc804
moddyz <moddy3d@gmail.com>
2020-10-23 00:01:16 +1300

    Added UsdSkel authoring blendshapes example.

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,4 +1,5 @@
 # Project options.
 
 option(BUILD_TESTING "Build & run automated tests." OFF)
+option(ENABLE_PYTHON_SUPPORT "Enable support for python." ON)
 option(USE_PYTHON_3 "Build python bindings for Python 3" OFF)
