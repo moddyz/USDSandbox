e4bcc33401576717687fe764308d5c2c18e547a0
moddyz <moddy3d@gmail.com>
2020-10-23 15:52:09 +1300

    pythonSnippets category

---
+++ src/pythonSnippets/usdGeomXformOps.py
@@ -0,0 +1,11 @@
+from pxr import Usd, UsdGeom, Gf
+
+
+if __name__ == "__main__":
+    # Create a new stage.
+    stage = Usd.Stage.CreateInMemory()
+    sphere = UsdGeom.Sphere.Define(stage, '/Spher')
+    sphere.AddTranslateOp().Set((10, 20, 30))
+    sphere.AddScaleOp().Set((2, 2, 2))
+    sphere.AddRotateXYZOp().Set((50, 30, 30))
+    print stage.ExportToString()
