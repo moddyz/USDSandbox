6eb0986eccf19fda04cd1e119e32db12ac6054e5
moddyz <moddy3d@gmail.com>
2020-10-27 23:20:27 +1300

    Author implicit instances snippet

---
+++ src/usdSnippets/usdAuthorImplicitInstances.py
@@ -0,0 +1,19 @@
+from pxr import Usd, UsdGeom
+
+
+if __name__ == "__main__":
+    # Create a new stage.
+    stage = Usd.Stage.CreateInMemory()
+
+    xform = UsdGeom.Xform.Define(stage, '/World')
+    sphere = UsdGeom.Sphere.Define(stage, '/World/Sphere')
+
+    refWorldA = stage.OverridePrim("/refWorldA")
+    refWorldA.GetReferences().AddInternalReference("/World")
+    refWorldA.SetInstanceable(True)
+
+    refWorldB = stage.OverridePrim("/refWorldB")
+    refWorldB.GetReferences().AddInternalReference("/World")
+    refWorldB.SetInstanceable(True)
+
+    print stage.GetRootLayer().ExportToString()
