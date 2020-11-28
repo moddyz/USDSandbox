8422408c80aac0895514524f64e99e1ce1ceda34
moddyz <moddy3d@gmail.com>
2020-11-28 18:42:41 +1300

    Added Cube to usdMakeVisibility snippet to illustrate invising sibling when parent upgrades to visible.

--- src/usdSnippets/usdMakeVisibility.py
+++ src/usdSnippets/usdMakeVisibility.py
@@ -10,15 +10,20 @@ if __name__ == "__main__":
     # Author a sphere parented under a scope.
     scopePrim = UsdGeom.Scope.Define(stage, '/World')
     spherePrim = UsdGeom.Sphere.Define(stage, '/World/Sphere')
+    cubePrim = UsdGeom.Cube.Define(stage, '/World/Cube')
 
     # Edit World visibility.
     scopePrim.MakeVisible(1) # No-op (already visible)
     scopePrim.MakeInvisible(2) # World is now "invisible"
 
-    # Edit Sphere visibility.
-    spherePrim.MakeVisible(1) # No-op for this prim, but will make "World" visible.
-    spherePrim.MakeInvisible(2) # "Sphere" will be invisible at timecode 2.
+    # No-op for this prim, but will make "World" visible.
+    # "Cube" will become invisible at timecode 1 due to "World"
+    # originally being invisible at timecode 1.
+    spherePrim.MakeVisible(1)
+
+    # After the following operation, "Sphere" will be invisible at timecode 2.
     # ... since no visiblity is authored for "Sphere" at timecode 1, it will stay invisible for
     # the entire timecode range of this stage.
+    spherePrim.MakeInvisible(2)
 
     # stage.Export("makeVisibility.usda")
