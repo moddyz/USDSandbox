b685c21777553685fe3b2060b65bc1457df5bf45
moddyz <moddy3d@gmail.com>
2020-12-03 23:00:59 +1300

    Comments comments comments

--- src/usdSnippets/authorInternalReferences.py
+++ src/usdSnippets/authorInternalReferences.py
@@ -7,15 +7,17 @@ from pxr import Usd
 
 if __name__ == "__main__":
 
-    # Author a model with a mesh child.
+    # Author modelA, with some descendents.
     stage = Usd.Stage.CreateInMemory()
     stage.DefinePrim("/modelA", "Xform")
     stage.DefinePrim("/modelA/Geom", "Xform")
     stage.DefinePrim("/modelA/Geom/Shape", "Mesh")
 
+    # Author modelB, with an internal reference arc to modelA.
     model = stage.DefinePrim("/modelB", "Xform")
     model.GetPrim().GetReferences().AddInternalReference("/modelA")
 
+    # Assert internal reference composition results.
     assert stage.GetPrimAtPath("/modelB/Geom")
     assert stage.GetPrimAtPath("/modelB/Geom/Shape")
 
