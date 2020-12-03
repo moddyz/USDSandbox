4e2d0b16800516e8cceef851b03ba1f1a0288bc1
moddyz <moddy3d@gmail.com>
2020-12-03 22:16:02 +1300

    Use anonymous layers instead of serialized file layers.

--- src/usdSnippets/testExternalReferences.py
+++ src/usdSnippets/testExternalReferences.py
@@ -4,43 +4,25 @@ Testing a SdfLayer.GetExternalReferences query.
 
 from pxr import Usd, UsdGeom, Sdf
 
-import os
-import tempfile
-import shutil
-
-
-class TemporaryDirectory(object):
-
-    def __enter__(self):
-        self._tempDir = tempfile.mkdtemp()
-        return self._tempDir
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        shutil.rmtree(self._tempDir)
-
 
 if __name__ == "__main__":
 
-    with TemporaryDirectory() as tempDir:
-        # Author a USD layer.
-        stage = Usd.Stage.CreateNew(os.path.join(tempDir, "foo.usda"))
-        stage.SetDefaultPrim(stage.DefinePrim("/model", "Xform"))
-        stage.Save()
+    # Author a USD layer.
+    stageA = Usd.Stage.CreateInMemory()
+    stageA.SetDefaultPrim(stageA.DefinePrim("/model", "Xform"))
 
-        # Author another USD layer.
-        stage = Usd.Stage.CreateNew(os.path.join(tempDir, "bar.usda"))
-        stage.SetDefaultPrim(stage.DefinePrim("/model", "Xform"))
-        stage.Save()
+    # Author another USD layer.
+    stageB = Usd.Stage.CreateInMemory()
+    stageB.SetDefaultPrim(stageB.DefinePrim("/model", "Xform"))
 
-        # Reference the previous two USD layers into a new layer.
-        stage = Usd.Stage.CreateNew(os.path.join(tempDir, "baz.usda"))
-        model = UsdGeom.Xform.Define(stage, "/model")
-        model.GetPrim().GetReferences().AddReference(os.path.join(tempDir, "foo.usda"))
-        model.GetPrim().GetReferences().AddReference(os.path.join(tempDir, "bar.usda"))
+    # Reference the previous two USD layers into a new layer.
+    stageC = Usd.Stage.CreateInMemory()
+    model = UsdGeom.Xform.Define(stageC, "/model")
+    model.GetPrim().GetReferences().AddReference(stageA.GetRootLayer().identifier)
+    model.GetPrim().GetReferences().AddReference(stageB.GetRootLayer().identifier)
 
-        # Test GetExternalReferences result.
-        assert set(stage.GetRootLayer().GetExternalReferences()) == set([
-            os.path.join(tempDir, "foo.usda"),
-            os.path.join(tempDir, "bar.usda"),
-        ])
-
+    # Test GetExternalReferences result.
+    assert set(stageC.GetRootLayer().GetExternalReferences()) == set([
+        stageA.GetRootLayer().identifier,
+        stageB.GetRootLayer().identifier,
+    ])
